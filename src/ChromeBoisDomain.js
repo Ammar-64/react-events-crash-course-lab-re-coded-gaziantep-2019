75abe5fc1961b6933c5752272a220f8c60b508ff
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-08-08 05:50:24 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -27,11 +27,21 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-'
    */
+   resizeHandler = (event) => {
+     const keyCode = event.which;
+     if (keyCode === 97 || keyCode === 65 ){
+       resize('+')
+     } else if (keyCode === 115 || keyCode === 83 ){
+       resize('-')
+     }
+   }
 
   render() {
     return (
       <canvas
         onMouseMove={this.handleMouseMove}
+        onClick = {this.handleToggleCycling}
+        onKeyDown = {this.resizeHandler}
         width='900'
         height='600'
         tabIndex="0">
